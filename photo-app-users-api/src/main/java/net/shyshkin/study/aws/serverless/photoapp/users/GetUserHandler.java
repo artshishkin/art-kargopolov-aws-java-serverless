02edad7a4b8c9b41004acf7b0afe82a1806aa790
Art <d.art.shishkin@gmail.com>
2021-11-29 21:12:20 +0200

    48. Reading Request returning Response (#6)

--- photo-app-users-api/src/main/java/net/shyshkin/study/aws/serverless/photoapp/users/GetUserHandler.java
+++ photo-app-users-api/src/main/java/net/shyshkin/study/aws/serverless/photoapp/users/GetUserHandler.java
@@ -4,10 +4,26 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
 
 public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
-        return null;
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+
+        String userId = input.getPathParameters().get("userId");
+
+        JsonObject returnValue = new JsonObject();
+        returnValue.addProperty("firstName", "Art");
+        returnValue.addProperty("lastNAme", "Shyshkin");
+        returnValue.addProperty("userId", userId);
+
+        var responseEvent = new APIGatewayProxyResponseEvent();
+
+        responseEvent
+                .withStatusCode(200)
+                .withBody(returnValue.toString());
+
+        return responseEvent;
     }
 }
