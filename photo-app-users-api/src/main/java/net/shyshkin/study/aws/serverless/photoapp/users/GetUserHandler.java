4322d4e52fa480264e7fbb8914e0916b2e4c8ed8
Art <d.art.shishkin@gmail.com>
2021-11-29 20:58:31 +0200

    47. Creating Lambda function (#6)

---
+++ photo-app-users-api/src/main/java/net/shyshkin/study/aws/serverless/photoapp/users/GetUserHandler.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.aws.serverless.photoapp.users;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        return null;
+    }
+}
