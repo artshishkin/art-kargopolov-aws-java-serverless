ec7477c670f8cd87d4198cc13ff26640b8fa765a
Art <d.art.shishkin@gmail.com>
2021-12-05 23:21:26 +0200

    138. Creating CognitoUserService and CreateUser function (#16)

---
+++ photo-app-users-api-cognito/PhotoAppUsersApiCognito/src/main/java/net/shyshkin/study/aws/serverless/cognito/service/CognitoUserService.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.aws.serverless.cognito.service;
+
+import com.google.gson.JsonObject;
+
+public class CognitoUserService {
+
+    public JsonObject createUser(JsonObject user) {
+        JsonObject storedUserDetails = new JsonObject();
+        return storedUserDetails;
+    }
+
+}
