a73b9045cc4ed77966a49fce400f8bd3741f7512
Art <d.art.shishkin@gmail.com>
2021-12-06 11:50:30 +0200

    142. Calling the createUser() function (#16)

--- photo-app-users-api-cognito/PhotoAppUsersApiCognito/src/main/java/net/shyshkin/study/aws/serverless/cognito/CreateUserHandler.java
+++ photo-app-users-api-cognito/PhotoAppUsersApiCognito/src/main/java/net/shyshkin/study/aws/serverless/cognito/CreateUserHandler.java
@@ -1,10 +1,14 @@
 package net.shyshkin.study.aws.serverless.cognito;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import net.shyshkin.study.aws.serverless.cognito.service.CognitoUserService;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,22 +18,43 @@ import java.util.Map;
  */
 public class CreateUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private final CognitoUserService cognitoUserService;
+    private final String appClientId;
+    private final String appClientSecret;
+
+    public CreateUserHandler() {
+        this.cognitoUserService = new CognitoUserService(System.getenv("AWS_REGION"));
+        this.appClientId = System.getenv("MY_COGNITO_POOL_APP_CLIENT_ID");
+        this.appClientSecret = System.getenv("MY_COGNITO_POOL_APP_CLIENT_SECRET");
+    }
+
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
         String requestBody = input.getBody();
 
-        context.getLogger().log("Request body: " + requestBody);
+        LambdaLogger logger = context.getLogger();
+        logger.log("Request body: " + requestBody);
 
         var userDetails = JsonParser.parseString(requestBody).getAsJsonObject();
 
+
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
-
-        return response
-                .withStatusCode(200)
-                .withBody("{}");
+        try {
+            JsonObject createUserResult = cognitoUserService.createUser(userDetails, appClientId, appClientSecret);
+            response
+                    .withStatusCode(200)
+                    .withBody(createUserResult.toString());
+        }catch (AwsServiceException ex){
+            String errorMessage = ex.awsErrorDetails().errorMessage();
+            logger.log(errorMessage);
+            response
+                    .withStatusCode(500)
+                    .withBody(errorMessage);
+        }
+        return response;
     }
 
 }
