ded9ba5ec60127381fb9b9d63ed0c22728dfb28b
Art <d.art.shishkin@gmail.com>
2021-12-06 16:35:05 +0200

    144. Creating the Confirm User Lambda function (#17 Section 18: Amazon Cognito- Implementing Confirm User Account)

---
+++ photo-app-users-api-cognito/PhotoAppUsersApiCognito/src/main/java/net/shyshkin/study/aws/serverless/cognito/ConfirmUserHandler.java
@@ -0,0 +1,14 @@
+package net.shyshkin.study.aws.serverless.cognito;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class ConfirmUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        var response = new APIGatewayProxyResponseEvent();
+        return response;
+    }
+}
