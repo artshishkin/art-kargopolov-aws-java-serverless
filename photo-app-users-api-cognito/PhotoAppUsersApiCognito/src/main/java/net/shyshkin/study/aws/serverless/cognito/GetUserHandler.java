ed1c4dc93d4e4afcfa1ea82cd40588a38a233857
Art <d.art.shishkin@gmail.com>
2021-12-08 19:37:10 +0200

    169. Calling the Get User method (#24)

--- photo-app-users-api-cognito/PhotoAppUsersApiCognito/src/main/java/net/shyshkin/study/aws/serverless/cognito/GetUserHandler.java
+++ photo-app-users-api-cognito/PhotoAppUsersApiCognito/src/main/java/net/shyshkin/study/aws/serverless/cognito/GetUserHandler.java
@@ -4,6 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+import net.shyshkin.study.aws.serverless.cognito.service.CognitoUserService;
+import net.shyshkin.study.aws.serverless.cognito.service.SerializerService;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
 
 import java.util.Map;
 
@@ -13,8 +17,38 @@ public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEven
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
         var response = new APIGatewayProxyResponseEvent()
                 .withHeaders(Map.of("Content-Type", "application/json"));
-        response.withBody("{\"status\": \"Working...\"}")
-                .withStatusCode(200);
+
+        var logger = context.getLogger();
+
+        try {
+
+            String accessToken = input.getHeaders().get("AccessToken");
+            JsonObject getUserResult = CognitoUserService.instance().getUser(accessToken);
+
+            response.withBody(getUserResult.toString())
+                    .withStatusCode(getUserResult.get("statusCode").getAsInt());
+
+        } catch (AwsServiceException ex) {
+            String errorMessage = ex.awsErrorDetails().errorMessage();
+            logger.log(errorMessage);
+
+            var errorResponse = new ErrorResponse(errorMessage);
+
+            var respBody = SerializerService.instance().toJson(errorResponse);
+            response
+                    .withStatusCode(ex.statusCode())
+                    .withBody(respBody);
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            logger.log(errorMessage);
+
+            var errorResponse = new ErrorResponse(errorMessage);
+
+            var respBody = SerializerService.instance().toJson(errorResponse);
+            response
+                    .withStatusCode(500)
+                    .withBody(respBody);
+        }
         return response;
     }
 }
