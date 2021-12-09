823835d3245c0b3042b00968c6987071dab37313
Art <d.art.shishkin@gmail.com>
2021-12-09 12:00:56 +0200

    174.1 Starter project - init(#25 Section 22: Lambda Authorizer - Creating custom Authorizer)

---
+++ lambda-authorizer/LambdaAuthorizer/src/test/java/net/shyshkin/study/aws/serverless/authorizer/GetUserByUsernameHandlerTest.java
@@ -0,0 +1,21 @@
+package net.shyshkin.study.aws.serverless.authorizer;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GetUserByUsernameHandlerTest {
+  @Test
+  public void successfulResponse() {
+    GetUserByUsernameHandler getUserByUsernameHandler = new GetUserByUsernameHandler();
+    APIGatewayProxyResponseEvent result = getUserByUsernameHandler.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+    assertNotNull(content);
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
