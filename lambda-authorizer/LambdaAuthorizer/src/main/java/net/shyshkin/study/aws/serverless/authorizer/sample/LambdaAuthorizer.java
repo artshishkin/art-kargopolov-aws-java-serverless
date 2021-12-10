d9b9dd2ce08dd97bf515e3f21d5bdca787cc733d
Art <d.art.shishkin@gmail.com>
2021-12-10 18:47:48 +0200

    178. Generating Policy Document (#25)

--- lambda-authorizer/LambdaAuthorizer/src/main/java/net/shyshkin/study/aws/serverless/authorizer/sample/LambdaAuthorizer.java
+++ lambda-authorizer/LambdaAuthorizer/src/main/java/net/shyshkin/study/aws/serverless/authorizer/sample/LambdaAuthorizer.java
@@ -3,14 +3,49 @@ package net.shyshkin.study.aws.serverless.authorizer.sample;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+import java.util.List;
+
+public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEvent, AuthorizerOutput> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+    public AuthorizerOutput handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 
-        return new APIGatewayProxyResponseEvent();
+        var requestContext = input.getRequestContext();
+
+//        String arn = "*"; //allow any resource
+        String arn = String.format(
+                "arn:aws:execute-api:%s:%s:%s/%s/%s/%s",
+                System.getenv("AWS_REGION"),
+                requestContext.getAccountId(),
+                requestContext.getApiId(),
+                requestContext.getStage(),
+                requestContext.getHttpMethod(),
+                "*"
+        );
+
+        String userName = input.getPathParameters().get("userName");
+
+        String effect = "Allow";
+        if ("123".equals(userName)) {
+            effect = "Deny";
+        }
+
+        var statement = Statement.builder()
+                .action("execute-api:Invoke")
+                .effect(effect)
+                .resource(arn)
+                .build();
+
+        var policyDocument = PolicyDocument.builder()
+                .version("2012-10-17")
+                .statements(List.of(statement))
+                .build();
+
+        return AuthorizerOutput.builder()
+                .policyDocument(policyDocument)
+                .principalId(userName)
+                .build();
     }
 
 }
