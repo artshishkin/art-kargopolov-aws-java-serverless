f3db15d1e99e44ac1adeeb1ca1c52559550ac674
Art <d.art.shishkin@gmail.com>
2021-12-10 23:53:35 +0200

    183. Validating the JWT (#25)

--- lambda-authorizer/LambdaAuthorizer/src/main/java/net/shyshkin/study/aws/serverless/authorizer/sample/LambdaAuthorizer.java
+++ lambda-authorizer/LambdaAuthorizer/src/main/java/net/shyshkin/study/aws/serverless/authorizer/sample/LambdaAuthorizer.java
@@ -3,6 +3,8 @@ package net.shyshkin.study.aws.serverless.authorizer.sample;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import net.shyshkin.study.aws.serverless.authorizer.utils.JwtUtils;
 
 import java.util.List;
 
@@ -14,9 +16,10 @@ public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEv
         var requestContext = input.getRequestContext();
 
 //        String arn = "*"; //allow any resource
+        String awsRegion = System.getenv("AWS_REGION");
         String arn = String.format(
                 "arn:aws:execute-api:%s:%s:%s/%s/%s/%s",
-                System.getenv("AWS_REGION"),
+                awsRegion,
                 requestContext.getAccountId(),
                 requestContext.getApiId(),
                 requestContext.getStage(),
@@ -27,8 +30,23 @@ public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEv
         String userName = input.getPathParameters().get("userName");
 
         String effect = "Allow";
-        if ("123".equals(userName)) {
+
+        JwtUtils jwtUtils = new JwtUtils();
+
+        String jwt = input.getHeaders()
+                .get("Authorization")
+                .replace("Bearer ", "")
+                .trim();
+
+        String userPoolId = System.getenv("PHOTO_APP_USERS_POOL_ID");
+        String audience = System.getenv("PHOTO_APP_USERS_APP_CLIENT_ID");
+        DecodedJWT decodedJWT = null;
+        try {
+            decodedJWT = jwtUtils.validateJwtForUser(jwt, awsRegion, userPoolId, userName, audience);
+            userName = decodedJWT.getSubject();
+        } catch (RuntimeException ex) {
             effect = "Deny";
+            ex.printStackTrace();
         }
 
         var statement = Statement.builder()
