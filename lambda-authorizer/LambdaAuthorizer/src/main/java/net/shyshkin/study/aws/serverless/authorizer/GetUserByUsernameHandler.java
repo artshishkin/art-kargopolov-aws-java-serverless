4906e871da6fef6d1b9bb71f9b73d04f3b85d9eb
Art <d.art.shishkin@gmail.com>
2021-12-12 12:21:06 +0200

    188. Calling getUserByUsername() from handleRequest() method (#25)

--- lambda-authorizer/LambdaAuthorizer/src/main/java/net/shyshkin/study/aws/serverless/authorizer/GetUserByUsernameHandler.java
+++ lambda-authorizer/LambdaAuthorizer/src/main/java/net/shyshkin/study/aws/serverless/authorizer/GetUserByUsernameHandler.java
@@ -4,15 +4,42 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+import net.shyshkin.study.aws.serverless.authorizer.service.CognitoUserService;
+
+import java.util.Map;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class GetUserByUsernameHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private final CognitoUserService cognitoUserService;
+
+    public GetUserByUsernameHandler() {
+        this.cognitoUserService = new CognitoUserService(System.getenv("AWS_REGION"));
+    }
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        return new APIGatewayProxyResponseEvent();
+
+        String userName = input.getPathParameters().get("userName");
+
+        String poolId = System.getenv("PHOTO_APP_USERS_POOL_ID");
+        try {
+            JsonObject userDetails = this.cognitoUserService.getUserByUsername(userName, poolId);
+
+            return new APIGatewayProxyResponseEvent()
+                    .withBody(userDetails.toString())
+                    .withStatusCode(200)
+                    .withHeaders(Map.of("Content-Type", "application/json"));
+        } catch (Exception ex) {
+            context.getLogger().log(ex.getMessage());
+            return new APIGatewayProxyResponseEvent()
+                    .withBody("{\"message\":\"" + ex.getMessage() + "\"}")
+                    .withStatusCode(500)
+                    .withHeaders(Map.of("Content-Type", "application/json"));
+        }
     }
 
 }
