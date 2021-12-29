d74ee1bf429d6e52110da2c6dbd6443984a11672
Art <d.art.shishkin@gmail.com>
2021-12-29 13:45:51 +0200

    202. Unit Test Lifecycle methods (#29)

--- photo-app-users-api-cognito/PhotoAppUsersApiCognito/lambdas/src/test/java/net/shyshkin/study/aws/serverless/cognito/CreateUserHandlerTest.java
+++ photo-app-users-api-cognito/PhotoAppUsersApiCognito/lambdas/src/test/java/net/shyshkin/study/aws/serverless/cognito/CreateUserHandlerTest.java
@@ -1,11 +1,30 @@
 package net.shyshkin.study.aws.serverless.cognito;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 class CreateUserHandlerTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After Each");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
 
     @Test
     @Disabled
