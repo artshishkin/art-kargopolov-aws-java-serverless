2a761c4eae732e4779f93ca8ef0062f292dbb7ce
Art <d.art.shishkin@gmail.com>
2021-12-29 13:33:25 +0200

    199. Running and failing a test case (#29)

---
+++ photo-app-users-api-cognito/PhotoAppUsersApiCognito/lambdas/src/test/java/net/shyshkin/study/aws/serverless/cognito/CreateUserHandlerTest.java
@@ -0,0 +1,15 @@
+package net.shyshkin.study.aws.serverless.cognito;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class CreateUserHandlerTest {
+
+    @Test
+    void failingTest() {
+
+        fail("Intentionally failing test case");
+
+    }
+}
