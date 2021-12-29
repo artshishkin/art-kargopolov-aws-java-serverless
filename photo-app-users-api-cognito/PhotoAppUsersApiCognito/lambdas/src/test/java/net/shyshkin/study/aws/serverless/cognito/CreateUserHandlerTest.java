eb779d71b02821a80afd3bd89122caaabecd29cb
Art <d.art.shishkin@gmail.com>
2021-12-29 13:37:51 +0200

    200. Naming a Unit Test Method (#29)

--- photo-app-users-api-cognito/PhotoAppUsersApiCognito/lambdas/src/test/java/net/shyshkin/study/aws/serverless/cognito/CreateUserHandlerTest.java
+++ photo-app-users-api-cognito/PhotoAppUsersApiCognito/lambdas/src/test/java/net/shyshkin/study/aws/serverless/cognito/CreateUserHandlerTest.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.aws.serverless.cognito;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -7,9 +8,20 @@ import static org.junit.jupiter.api.Assertions.fail;
 class CreateUserHandlerTest {
 
     @Test
+    @Disabled
     void failingTest() {
 
         fail("Intentionally failing test case");
+
+    }
+
+    @Test
+    void testHandleRequest_whenValidRequestProvided_thenShouldReturnSuccessfulResponse() {
+        //given
+
+        //when
+
+        //then
 
     }
 }
